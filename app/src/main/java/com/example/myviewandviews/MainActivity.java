4872d1ae087639d89e3d82c76ad5be97c7ef31cb
liemroym <liemroymarcelino@gmail.com>
2022-11-25 16:15:57 +0700

    Added intent and activity buy

--- app/src/main/java/com/example/myviewandviews/MainActivity.java
+++ app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myviewandviews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Arduino UNO");
+        }
+
+        // Inisialisasi onClickListener untuk button yang ada
+        Button callCsBtn = findViewById(R.id.dial_cs_btn);
+        Button buyBtn = findViewById(R.id.buy_btn);
+
+        callCsBtn.setOnClickListener(this);
+        buyBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.dial_cs_btn:
+                String phoneNumber = "08214166739";
+                Intent dialCSIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dialCSIntent);
+                break;
+            case R.id.buy_btn:
+                Intent buyIntent = new Intent(MainActivity.this, BuyActivity.class);
+                startActivity(buyIntent);
+                break;
         }
     }
 }
